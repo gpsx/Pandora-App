d6d8d7ccec9786da92289942ded9cb064597889f
gustavouesso <gustavouesso.gu@gmail.com>
2020-10-14 22:27:49 -0300

    added shared preferences

--- app/src/main/java/x/pandoraapp/views/LoginActivity.kt
+++ app/src/main/java/x/pandoraapp/views/LoginActivity.kt
@@ -1,8 +1,13 @@
 package x.pandoraapp.views
 
+import android.content.Context
 import android.content.Intent
+import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.preference.PreferenceManager
+import android.preference.PreferenceManager.getDefaultSharedPreferences
+import android.view.View
 import kotlinx.android.synthetic.main.activity_login.*
 import x.pandoraapp.R
 
@@ -14,10 +19,30 @@ class LoginActivity : AppCompatActivity() {
         enterButton.setOnClickListener {
             startActivity(MainActivity::class.java)
         }
+
+        val pref = getDefaultSharedPreferences(this);
+        pref.apply {
+            val username = getString("USERNAME", "")
+            val password = getString("PASSWORD", "")
+            emailEditText.setText(username)
+            passwordEditText.setText(password)
+        }
     }
     private fun<T> startActivity(activity : Class<T>){
         val intent = Intent(this, activity)
         this.startActivity(intent)
         this.finish()
     }
+
+
+    fun login(componente: View){
+        val pref = getDefaultSharedPreferences(this);
+
+        val editor = pref.edit();
+        editor
+            .putString("USERNAME", emailEditText.text.toString())
+            .putString("PASSWORD", passwordEditText.text.toString())
+            .apply()
+    }
+
 }
